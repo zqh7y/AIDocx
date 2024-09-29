e124f3070a69f537dad17a933d5895aad84da304
zqh7y❤️‍🔥 <113249259+JustAriel@users.noreply.github.com>
2024-09-29 23:49:31 +0300

    Add files via upload

---
+++ Paragraph.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text, TextInput, View } from 'react-native';
+import paragraphStyle from '../style/ParagraphStyle';
+
+const Paragraph = ({ setText }) => {
+  return (
+    <View style={paragraphStyle.container}>
+      <TextInput 
+        style={paragraphStyle.input}
+        placeholderTextColor="#222"
+        multiline={true}
+        textAlignVertical='top'
+        onChangeText={setText}
+      />
+      <Text style={paragraphStyle.aiText}>Your AI assistant.</Text>
+      <TextInput 
+        style={paragraphStyle.inputAi}
+        placeholderTextColor="#111"
+        multiline={true}
+        textAlignVertical='top' 
+      />
+    </View>
+  );
+};
+
+export default Paragraph;
